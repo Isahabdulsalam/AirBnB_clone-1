2279b424f565b73b32eb40421a74d4184d4335a4
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-04-18 00:06:48 +0100

    models

--- models/review.py
+++ models/review.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the review class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
-
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 class Review(BaseModel, Base):
     """This is the class for Review
