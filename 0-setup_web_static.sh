56c1ede97cbe2f0bfdf908e6fe653b5c5cdb2b8a
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-05-03 15:56:41 -0700

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,13 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -17,22 +23,17 @@ printf %s "server {
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
-    index  index.html index.htm;
-
+    index  index.html index.htm index.nginx-debian.html;
     location /hbnb_static {
         alias /data/web_static/current;
-        index index.html index.htm;
+        index index.html index.htm index.nginx-debian.html;
     }
-
     location /redirect_me {
         return 301 http://cuberule.com/;
     }
-
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
       internal;
     }
 }" > /etc/nginx/sites-available/default
-
-service nginx restart
